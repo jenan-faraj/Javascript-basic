b8704c4eece998f8369b4d447d9ddb35ef553cdd
jenan-faraj <jenan.faraj4@gmail.com>
2024-12-26 16:25:26 +0300

    my task

---
+++ README.md
@@ -0,0 +1,7 @@
+# Javascript-basic
+## This is my Javascript-basic
+### Author
+- jenan faraj
+## Live URL
+- 
+**Thank you so much for your time and effort in reviewing my task. I truly appreciate your support and guidance! 🙏🌟**
---
+++ main.js
@@ -0,0 +1,37 @@
+// Q1
+console.log(-5 * 3);
+console.log("JavaScript" + 50);
+console.log(17 % 5);
+console.log(5 % 17);
+console.log(5 / 10);
+console.log(4 === "4");
+console.log(4 != 5);
+console.log(7 <= 8);
+console.log("Hello" + 5);
+let x = 5;
+console.log(Math.ceil(x) - Math.floor(x)) ;
+console.log(Math.floor(x));
+
+// Q2
+let num = prompt("Enter a number:");
+alert(num);
+
+// Q3
+let num1 = prompt("Enter the first number:");
+let num2 = prompt("Enter the second number:");
+console.log(Number(num1) + Number(num2));
+
+// Q4
+let num3 = prompt("Enter the first number:");
+let num4 = prompt("Enter the second number:");
+console.log(Number(num3) * Number(num4));
+
+// Q5
+let num5 = prompt("Enter the first number:");
+let num6 = prompt("Enter the second number:");
+console.log(Number(num5) / Number(num6));
+
+// Q6
+let firstNum = 5;
+let secondNum = 10;
+console.log(firstNum + secondNum)
